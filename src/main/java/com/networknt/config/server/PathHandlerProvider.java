cd4e5ef41721cc384362080499c84d79da8a3286
Steve Hu <stevehu@gmail.com>
2017-03-02 11:18:35 -0500

    initial checkin after swagger-codegen

---
+++ src/main/java/com/networknt/config/server/PathHandlerProvider.java
@@ -0,0 +1,21 @@
+package com.networknt.config.server;
+
+import com.networknt.config.Config;
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
+import com.networknt.info.ServerInfoGetHandler;
+import com.networknt.config.server.handler.*;
+
+public class PathHandlerProvider implements HandlerProvider {
+    @Override
+    public HttpHandler getHandler() {
+        return Handlers.routing()
+            .add(Methods.GET, "/v1/config/{service}/{profile}/{name}", new ConfigServiceProfileNameGetHandler())
+            .add(Methods.GET, "/v1/server/info", new ServerInfoGetHandler())
+        ;
+    }
+}
+
