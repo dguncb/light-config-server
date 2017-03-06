a8daa2e17fb7af6e18d45f1bbac883230f8338e5
Steve Hu <stevehu@gmail.com>
2017-03-05 21:04:34 -0500

    create zip file and clean working folder

--- src/main/java/com/networknt/config/server/handler/ConfigVersionProfileServiceGetHandler.java
+++ src/main/java/com/networknt/config/server/handler/ConfigVersionProfileServiceGetHandler.java
@@ -1,15 +1,22 @@
 package com.networknt.config.server.handler;
 
 import com.networknt.config.Config;
+import com.networknt.config.server.repository.CopyFileVisitor;
+import com.networknt.config.server.repository.DeleteFileVisitor;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PullResult;
@@ -33,6 +40,7 @@ public class ConfigVersionProfileServiceGetHandler implements HttpHandler {
     static final String ABSOLUTE_WORKING = BASE_FLODER + WORKING_FOLDER;
     static final String ABSOLUTE_REPOSITORIES = BASE_FLODER + REPOSITORIES_FOLDER;
     static final Map<String, Object> config = Config.getInstance().getJsonMapConfig(CONFIG_NAME);
+    static final String defaultRepoUri = (String)config.get("default_repo_uri");
 
     public ConfigVersionProfileServiceGetHandler() {
         // get the working directories to initialize them if necessary
@@ -42,13 +50,11 @@ public class ConfigVersionProfileServiceGetHandler implements HttpHandler {
         initializeDirectory(ABSOLUTE_REPOSITORIES);
 
         // check if default config repo is there. clone if it not there
-        String defaultRepo = (String)config.get("default_repo");
-        String repoName = defaultRepo.substring(defaultRepo.indexOf("/"), defaultRepo.indexOf(".git"));
-        File file = new File(ABSOLUTE_REPOSITORIES + repoName);
+        File file = new File(ABSOLUTE_REPOSITORIES + getRepoName(defaultRepoUri));
         if(!file.exists()) {
             // clone
             try (Git result = Git.cloneRepository()
-                    .setURI(defaultRepo)
+                    .setURI(defaultRepoUri)
                     .setDirectory(file)
                     .call()) {
                 // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
@@ -82,6 +88,9 @@ public class ConfigVersionProfileServiceGetHandler implements HttpHandler {
         }
     }
 
+    private String getRepoName(String repoUri) {
+        return repoUri.substring(repoUri.indexOf("/"), repoUri.indexOf(".git"));
+    }
     private void initializeDirectory(String absPath) {
         // check if it exists
         File file = new File(absPath);
@@ -107,6 +116,35 @@ public class ConfigVersionProfileServiceGetHandler implements HttpHandler {
         }
     }
 
+    public void addDirToZipArchive(ZipOutputStream zos, File fileToZip, String parrentDirectoryName) throws Exception {
+        if (fileToZip == null || !fileToZip.exists()) {
+            return;
+        }
+
+        String zipEntryName = fileToZip.getName();
+        if (parrentDirectoryName!=null && !parrentDirectoryName.isEmpty()) {
+            zipEntryName = parrentDirectoryName + "/" + fileToZip.getName();
+        }
+
+        if (fileToZip.isDirectory()) {
+            System.out.println("+" + zipEntryName);
+            for (File file : fileToZip.listFiles()) {
+                addDirToZipArchive(zos, file, zipEntryName);
+            }
+        } else {
+            System.out.println("   " + zipEntryName);
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = new FileInputStream(fileToZip);
+            zos.putNextEntry(new ZipEntry(zipEntryName));
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                zos.write(buffer, 0, length);
+            }
+            zos.closeEntry();
+            fis.close();
+        }
+    }
+
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
 
@@ -117,8 +155,29 @@ public class ConfigVersionProfileServiceGetHandler implements HttpHandler {
         if(logger.isDebugEnabled()) logger.debug("version = " + version + " profile = " + profile + " service = " + service);
 
         // concat file name
-        String path = ABSOLUTE_WORKING + "/" + version + "/" + profile + "/" + service + "/config.zip";
-        if(logger.isDebugEnabled()) logger.debug("path " + path);
+        String sourceFolder = ABSOLUTE_REPOSITORIES + getRepoName(defaultRepoUri) + "/" + version;
+        String targetFolder = ABSOLUTE_WORKING + "/" + version + "/" + profile + "/" + service + "/config";
+        String zipFile = ABSOLUTE_WORKING + "/" + version + "/" + profile + "/" + service + "/config.zip";
+        // try to find the file and if it doesn't exist, create it by merging several repos.
+        if(logger.isDebugEnabled()) logger.debug("zipFile " + zipFile);
+        Path path = Paths.get(zipFile);
+        if (!Files.exists(path)) {
+            // create a zip from repos.
+            if(!Files.exists(Paths.get(targetFolder))) {
+                Files.createDirectories(Paths.get(targetFolder));
+            }
+            logger.debug("sourceFolder = " + sourceFolder);
+            logger.debug("targetFolder = " + targetFolder);
+            Files.walkFileTree(Paths.get(sourceFolder), new CopyFileVisitor(Paths.get(targetFolder)));
+            try (FileOutputStream fos = new FileOutputStream(zipFile)) {
+                try(ZipOutputStream zos = new ZipOutputStream(fos)) {
+                    addDirToZipArchive(zos, new File(targetFolder), null);
+                    zos.flush();
+                    fos.flush();
+                }
+            }
+            Files.walkFileTree(Paths.get(targetFolder), new DeleteFileVisitor());
+        }
         exchange.getResponseSender().send("OK");
 
     }
