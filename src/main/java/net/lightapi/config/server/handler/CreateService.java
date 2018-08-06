71519ea5eb945fb0a29eaf4672f28e48785c5826
Gavin Chen <gavinyan.han@gmail.com>
2018-08-06 11:22:35 -0400

    fix project build error

--- src/main/java/net/lightapi/config/server/handler/CreateService.java
+++ src/main/java/net/lightapi/config/server/handler/CreateService.java
@@ -1,16 +1,38 @@
 
 package net.lightapi.config.server.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.service.SingletonServiceFactory;
 import com.networknt.utility.NioUtils;
 import com.networknt.rpc.Handler;
 import com.networknt.rpc.router.ServiceHandler;
 import java.nio.ByteBuffer;
 import io.undertow.server.HttpServerExchange;
+import net.lightapi.config.server.common.ConfigService;
+import net.lightapi.config.server.jdbc.ConfigRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ServiceHandler(id="lightapi.net/config/create-service/0.1.0")
 public class CreateService implements Handler {
+    static final Logger logger = LoggerFactory.getLogger(CreateService.class);
+    private ConfigRepository configRepository = (ConfigRepository) SingletonServiceFactory.getBean(ConfigRepository.class);
     @Override
     public ByteBuffer handle(HttpServerExchange exchange, Object input)  {
-        return NioUtils.toByteBuffer("");
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String result;
+
+        try {
+            String json = mapper.writeValueAsString(input);
+            ConfigService configService = mapper.readValue(json, ConfigService.class);
+            result = configRepository.createConfigService(configService);
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+
+        return NioUtils.toByteBuffer(result);
     }
 }
--- src/test/java/net/lightapi/config/server/handler/CreateCommonValuesTest.java
+++ src/test/java/net/lightapi/config/server/handler/CreateCommonValuesTest.java
@@ -22,11 +22,11 @@ import java.net.URI;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class CreateServiceValues Test {
+public class CreateCommonValuesTest {
     @ClassRule
     public static TestServer server = TestServer.getInstance();
 
-    static final Logger logger = LoggerFactory.getLogger(CreateServiceValues .class); 
+    static final Logger logger = LoggerFactory.getLogger(CreateCommonValues .class); 
     static final boolean enableHttp2 = server.getServerConfig().isEnableHttp2();
     static final boolean enableHttps = server.getServerConfig().isEnableHttps();
     static final int httpPort = server.getServerConfig().getHttpPort();
@@ -34,7 +34,7 @@ public class CreateServiceValues Test {
     static final String url = enableHttp2 || enableHttps ? "https://localhost:" + httpsPort : "http://localhost:" + httpPort;
 
     @Test
-    public void testCreateServiceValues () throws ClientException, ApiException {
+    public void testCreateCommonValues () throws ClientException, ApiException {
         /*
         final Http2Client client = Http2Client.getInstance();
         final CountDownLatch latch = new CountDownLatch(1);
